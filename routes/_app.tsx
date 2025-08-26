0850cb751bb2e8ba0f1a34c63f9901c9a45b1458
FlyingFool95 <jhernehult@gmail.com>
2025-08-26 21:30:07 +0200

    Cleaning up files - Adding default styling - Adding fonts

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,17 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
-  return (
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>sollys</title>
-        <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>
-        <Component />
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Sollys - Every day a little brighter</title>
+                <link rel="stylesheet" href="/fonts/fonts.css" />
+                <link rel="stylesheet" href="/styles.css" />
+            </head>
+            <body>
+                <Component />
+            </body>
+        </html>
+    );
 }
