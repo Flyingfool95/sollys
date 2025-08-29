09b7f76dc227a8c1af46c1d706a3002853f8d9b7
FlyingFool95 <jhernehult@gmail.com>
2025-08-29 12:21:54 +0200

    Adding Feedback component

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,3 +1,4 @@
+import Feedback from "../components/feedback/Feedback.tsx";
 import Footer from "../components/footer/Footer.tsx";
 import Header from "../components/header/Header.tsx";
 import Dashboard from "../islands/Dashboard.tsx";
@@ -6,15 +7,12 @@ export default function Home() {
     return (
         <div>
             <Header />
-            <Dashboard />
 
-            <hr />
-            <h2>Feedback</h2>
-            <form method="post">
-                <textarea name="feedback" id="feedback" placeholder="My feedback goes here..."></textarea>
-                <input type="submit" value="Send" />
-            </form>
-
+            <main>
+                <Dashboard />
+                <hr />
+                <Feedback />
+            </main>
             <Footer />
         </div>
     );
