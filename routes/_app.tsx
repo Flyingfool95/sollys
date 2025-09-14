c44730366cd7e0c4f51018b770f29dae279c4777
Flyingfool95 <jhernehult@gmail.com>
2025-09-14 17:20:45 +0200

    prefetching critical styling

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,21 +1,17 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
     return (
-        <html lang="en">
+        <html>
             <head>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="theme-color" content="#001a2d" />
                 <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
                 <title>Sollys - Every day a little brighter</title>
-                <meta
-                    name="description"
-                    content="Project Sollys is a PWA that helps northern residents make the most of their days by showing sunrise, sunset, daylight duration, and daily light-based tips."
-                />
                 {/* Global */}
-                <link rel="preload" href="/fonts/fonts.css" />
-                <link rel="preload" href="/styles/variables.css" />
-                <link rel="preload" href="/styles/styles.css" />
+                <link rel="prefetch" href="/fonts/fonts.css" />
+                <link rel="prefetch" href="/styles/variables.css" />
+                <link rel="prefetch" href="/styles/styles.css" />
                 {/* Components */}
                 <link rel="stylesheet" href="/styles/components/header.css" />
                 <link rel="stylesheet" href="/styles/components/logo.css" />
