8ea6ff0c13c4ed708c0ae10cf2d593efbf796084
Flyingfool95 <jhernehult@gmail.com>
2025-09-14 21:40:37 +0200

    aria hidden on logo

--- components/header/Logo.tsx
+++ components/header/Logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
     return (
-        <div className="logo" aria-label="My company logo">
+        <div className="logo" aria-hidden="true">
             <div className="bar"></div>
             <div className="bar"></div>
             <div className="bar"></div>
