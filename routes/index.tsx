e9407d25c044017ddfe80c98e89eb9d59548a7b5
Flyingfool95 <jhernehult@gmail.com>
2025-09-14 11:29:35 +0200

    commenting out feedback component

--- routes/index.tsx
+++ routes/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <main>
                 <Dashboard />
                 <hr />
-                <Feedback />
+                {/* <Feedback /> */}
             </main>
             <Footer />
         </>
