cc6888706435fcd1b8a377b448f7f58a3046b985
FlyingFool95 <jhernehult@gmail.com>
2025-10-14 20:31:02 +0200

    Adding daily tip to be cahced on device and reset on midnight. - Adding helper to callculate ms until midnight - Adding default list of tips for now.

--- islands/Tip.tsx
+++ islands/Tip.tsx
@@ -5,7 +5,7 @@ export default function Tip() {
         <div className="tip">
             <h2>{todaysTip.value?.title}</h2>
             <p>{todaysTip.value?.tip}</p>
-            <a href={todaysTip.value?.source}>- Source to tip</a>
+            <a href={todaysTip.value?.source} target="_blank">- Source</a>
         </div>
     );
 }
--- signals/tips.signal.ts
+++ signals/tips.signal.ts
@@ -1,4 +1,5 @@
-import { signal } from "@preact/signals";
+import { persistentSignal } from "../helpers/global.helpers.ts";
 import { Tip } from "../types/serverData.types.ts";
+import { msUntilNextMidnight } from "../helpers/global.helpers.ts";
 
-export const todaysTip = signal<Tip | undefined>(undefined);
+export const todaysTip = persistentSignal<Tip | undefined>("todays-tip", undefined, msUntilNextMidnight());
