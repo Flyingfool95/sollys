7ed150c77ed26a80a875a45ebe285aa4224c5b4d
Flyingfool95 <jhernehult@gmail.com>
2025-10-16 14:45:22 +0200

    extracting DashboardData type out of function

--- islands/Dashboard.tsx
+++ islands/Dashboard.tsx
@@ -1,29 +1,29 @@
-export default function Dashboard({ data }: {
-	data: {
-		sunrise: {
-			name: string;
-			hours: number;
-			minutes: number;
-			date: Date;
-		};
-		sunset: {
-			name: string;
-			hours: number;
-			minutes: number;
-			date: Date;
-		};
-		duration: {
-			name: string;
-			hours: number;
-			minutes: number;
-		};
-		nextEvent: {
-			name: string;
-			hours: number;
-			minutes: number;
-		};
+export type DashboardData = {
+	sunrise: {
+		name: string;
+		hours: number;
+		minutes: number;
+		date: Date;
 	};
-}) {
+	sunset: {
+		name: string;
+		hours: number;
+		minutes: number;
+		date: Date;
+	};
+	duration: {
+		name: string;
+		hours: number;
+		minutes: number;
+	};
+	nextEvent: {
+		name: string;
+		hours: number;
+		minutes: number;
+	};
+};
+
+export default function Dashboard({ data }: { data: DashboardData }) {
 	console.log(data);
 	return <div>{data.sunrise.name}</div>;
 }
