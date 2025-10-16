000dec5edc0d03897fd56d9ba90e14ac2515beac
Flyingfool95 <jhernehult@gmail.com>
2025-10-16 14:44:16 +0200

    Fixing dashboard data calculations - tweaking deno.json -  Adding nextEvent to type for dashboard data

--- islands/Dashboard.tsx
+++ islands/Dashboard.tsx
@@ -17,6 +17,11 @@ export default function Dashboard({ data }: {
 			hours: number;
 			minutes: number;
 		};
+		nextEvent: {
+			name: string;
+			hours: number;
+			minutes: number;
+		};
 	};
 }) {
 	console.log(data);
--- lib/helpers/sunData.helper.ts
+++ lib/helpers/sunData.helper.ts
@@ -3,8 +3,8 @@ import SunCalc from "npm:suncalc@1.9.0";
 const latitude = 55.6078;
 const longitude = 12.9982;
 
-export function getSunRise() {
-	const sunrise = SunCalc.getTimes(new Date(), latitude, longitude).sunrise;
+export function getSunrise(date = new Date()) {
+	const sunrise = SunCalc.getTimes(date, latitude, longitude).sunrise;
 	return {
 		name: "sunrise",
 		hours: sunrise.getHours(),
@@ -13,8 +13,8 @@ export function getSunRise() {
 	};
 }
 
-export function getSunSet() {
-	const sunset = SunCalc.getTimes(new Date(), latitude, longitude).sunset;
+export function getSunset(date = new Date()) {
+	const sunset = SunCalc.getTimes(date, latitude, longitude).sunset;
 	return {
 		name: "sunset",
 		hours: sunset.getHours(),
@@ -24,13 +24,10 @@ export function getSunSet() {
 }
 
 export function getDuration() {
-	const sunrise = getSunRise().date;
-	const sunset = getSunSet().date;
+	const sunrise = getSunrise().date;
+	const sunset = getSunset().date;
 
-	const diffMs = sunset - sunrise;
-	const diffMinutes = Math.floor(diffMs / 1000 / 60);
-	const hours = Math.floor(diffMinutes / 60);
-	const minutes = diffMinutes % 60;
+	const { hours, minutes } = getDeltaTime(sunrise, sunset);
 
 	return {
 		name: "duration",
@@ -39,8 +36,50 @@ export function getDuration() {
 	};
 }
 
+export function getNextEvent() {
+	const currentTime = new Date();
+	const tomorrowsSunrise = getSunrise(new Date(Date.now() + 24 * 60 * 60 * 1000)).date;
+	const sunset = getSunset().date;
+
+	let hours: number;
+	let minutes: number;
+	let eventName: string;
+
+	if (currentTime < sunset) {
+		const times = getDeltaTime(currentTime, sunset);
+		hours = times.hours;
+		minutes = times.minutes;
+		eventName = "sunset";
+	} else {
+		const times = getDeltaTime(currentTime, tomorrowsSunrise);
+		hours = times.hours;
+		minutes = times.minutes;
+		eventName = "sunrise";
+	}
+
+	return {
+		name: "nextEvent",
+		hours,
+		minutes,
+		eventName,
+	};
+}
+
+export function getDeltaTime(startTime: Date, endTime: Date) {
+	const diffMs = endTime.valueOf() - startTime.valueOf();
+	const diffMinutes = Math.floor(diffMs / 1000 / 60);
+	const hours = Math.floor(diffMinutes / 60);
+	const minutes = diffMinutes % 60;
+
+	return {
+		hours,
+		minutes,
+	};
+}
+
 export const sunData = {
-	sunrise: getSunRise(),
-	sunset: getSunSet(),
+	sunrise: getSunrise(),
+	sunset: getSunset(),
 	duration: getDuration(),
+	nextEvent: getNextEvent(),
 };
